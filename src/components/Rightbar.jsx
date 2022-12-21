1cf7d4ff67095c11f557380f511d6a6d0a8fdb28
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2022-12-21 23:46:10 +0100

    Made the Rightbar scrollable

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,10 +1,16 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
 
 function Rightbar() {
   return (
-    <Box bgcolor="orange" flex={2} p={2} sx={{display:{xs:"none ", sm: "block"}}}>Rightbar</Box>
-  )
+    <Box
+      flex={2}
+      p={2}
+      sx={{ display: { xs: "none ", sm: "block" } }}
+    >
+      <Box sx={{ position: "fixed" }}>Rightbar</Box>
+    </Box>
+  );
 }
 
-export default Rightbar
+export default Rightbar;
