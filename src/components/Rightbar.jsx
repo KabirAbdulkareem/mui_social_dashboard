b47306fe49426818a9d3846078e8ffa354d42a5e
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2022-12-22 09:28:56 +0100

    Added online friend avartars to the side bar

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,14 +1,56 @@
-import { Box } from "@mui/material";
+import { Avatar, AvatarGroup, Box, Typography } from "@mui/material";
 import React from "react";
 
 function Rightbar() {
   return (
-    <Box
-      flex={2}
-      p={2}
-      sx={{ display: { xs: "none ", sm: "block" } }}
-    >
-      <Box sx={{ position: "fixed" }}>Rightbar</Box>
+    <Box flex={2} p={2} sx={{ display: { xs: "none ", sm: "block" } }}>
+      <Box sx={{ position: "fixed" }}>
+        <Typography variant="h6" fontWeight={100}>
+          Online Friends
+        </Typography>
+        <AvatarGroup max={7}>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://mui.com/static/images/avatar/1.jpg"
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://mui.com/static/images/avatar/2.jpg"
+          />
+          <Avatar
+            alt="Cindy Baker"
+            src="https://mui.com/static/images/avatar/3.jpg"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://mui.com/static/images/avatar/4.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://mui.com/static/images/avatar/5.jpg"
+          />
+          <Avatar
+            alt="Remy Sharp"
+            src="https://mui.com/static/images/avatar/6.jpg"
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://mui.com/static/images/avatar/7.jpg"
+          />
+          <Avatar
+            alt="Cindy Baker"
+            src="https://mui.com/static/images/avatar/8.jpg"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://mui.com/static/images/avatar/9.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://mui.com/static/images/avatar/10.jpg"
+          />
+        </AvatarGroup>
+      </Box>
     </Box>
   );
 }
