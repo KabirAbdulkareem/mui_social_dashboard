3541c94875410b981edbaadce08bf62cd3dce523
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2022-12-24 14:37:57 +0100

    Online friend component added

---
+++ src/components/OnlineFriends.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+
+const itemData = [
+    {
+      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      title: 'Breakfast',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+      title: 'Burger',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+      title: 'Camera',
+    },
+  ];
+
+function OnlineFriends() {
+  return (
+    <ImageList cols={3}  gap={5}>
+      {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  )
+}
+
+export default OnlineFriends
