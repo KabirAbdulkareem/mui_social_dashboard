35d57c30508fb53fa6fa31c44bae43635ebdc5f4
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2023-01-03 14:56:55 +0100

    Added Tooltip to the Add button

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
+import Tooltip from '@mui/material/Tooltip';
 
-const AddButton
- = () => {
+const AddButton = () => {
   return (
     <>
       <Box
@@ -14,13 +14,15 @@ const AddButton
           left: { xs: "calc(50% - 28px)", md: 30 },
         }}
       >
-        <Fab color="primary" aria-label="add">
-          <AddIcon />
-        </Fab>
-      </Box>    
+        <Tooltip title="Delete">
+          <Fab color="primary" aria-label="add">
+            <AddIcon />
+          </Fab>
+        </Tooltip>
+
+      </Box>
     </>
   );
 };
 
-export default AddButton
-;
+export default AddButton;
